e6cfd087fd5fec7d69cc6b0b08f5575f085442a5
daniyalyk <daniyalyk@gmail.com>
2020-11-02 20:05:41 +0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {ApiDataShow} from "./Apidatashow";
+
+function App() {
+  return (
+    <div className="App">
+     <ApiDataShow/>
+    </div>
+  );
+}
+
+export default App;
